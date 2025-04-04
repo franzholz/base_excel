7700a1492da7f7e1fcd27fc4497d7a52e0e00df4
Franz Holzinger <franz.holzinger@festwein.de>
2025-04-04 18:32:22 +0200

    fix #6 : version 4.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,14 +8,14 @@ $EM_CONF[$_EXTKEY] = [
     'title'            => 'Excel Library',
     'description'      => 'This provides the PhpSpreadsheet library, formerly phpExcel from phpOffice.',
     'category'         => 'misc',
-    'version'          => '3.7.0',
+    'version'          => '4.1.0',
     'state'            => 'stable',
     'author'           => 'PhpSpreadsheet developers, Franz Holzinger',
     'author_email'     => 'franz@ttproducts.de',
     'author_company'   => 'jambage.com',
     'constraints'      => [
         'depends'   => [
-            'typo3' => '11.5.0-13.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [
         ],
